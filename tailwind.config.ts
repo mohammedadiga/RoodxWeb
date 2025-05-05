63f61fbe323a032ba9a9821d72d062a8bc35826b
mohammedadiga <mohammed.adiga011@gmail.com>
2025-05-06 01:44:09 +0200

    feat(ui): install shadcn-ui and customize global styles

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+    screens:{
+      "1000px":"1000px",
+      "1100px":"1100px",
+      "1200px":"1200px",
+      "1300px":"1300px",
+      "1500px":"1500px",
+      "800px":"800px",
+      "400px":"400px",
+    }
+  },
+  plugins: [],
+};
+
+export default config
